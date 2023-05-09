b060c21503c925d4a47230175127c597db90b20c
Yijia Chen <kevinc110604@gmail.com>
2023-05-09 16:08:43 -0400

    [new] Store password with its hash, add /users API
    Complete L17

---
+++ util/password_test.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	// If the same password is hash twice, the two results should not be the same (bc of the salt)
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqual(t, hashedPassword, hashedPassword2)
+}
