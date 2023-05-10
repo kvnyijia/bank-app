0891668467039e0d660cdfe7f51c4be544341a7f
Yijia Chen <kevinc110604@gmail.com>
2023-05-09 20:28:51 -0400

    [new] Add PASETO validation
    Complete L20

---
+++ token/paseto_maker.go
@@ -0,0 +1,49 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+type PasetoMaker struct {
+	paseto        *paseto.V2
+	sysmmetricKey []byte
+}
+
+func NewPasetoMaker(sysmmetricKey string) (Maker, error) {
+	if len(sysmmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:        paseto.NewV2(),
+		sysmmetricKey: []byte(sysmmetricKey),
+	}
+	return maker, nil
+}
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.sysmmetricKey, payload, nil) // The last one is optional footer
+}
+
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.sysmmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
