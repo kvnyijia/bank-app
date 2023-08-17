f3df2c8337349c05cb50f62e38920bdfed7a9183
Yijia Chen <kevinc110604@gmail.com>
2023-08-17 11:12:50 -0700

    [fix] Use tx to createUser & sendVerifyEmail
    Completed L56

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -9,6 +9,7 @@ import (
 type Store interface {
 	Querier
 	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
 }
 
 // Provides all funcs to exec SQL queries & txs
