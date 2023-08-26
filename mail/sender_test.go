773ee11d342b3a83e9ae77bd845952e9e99a18ed
Yijia Chen <kevinc110604@gmail.com>
2023-08-25 18:03:21 -0700

    [fix] Use -short flag to skip the unit test
    Complete L60

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -1,26 +1,29 @@
 package mail
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/kvnyijia/bank-app/util"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/kvnyijia/bank-app/util"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestSendEmailWithGamil(t *testing.T) {
-// 	config, err := util.LoadConfig("..")
-// 	require.NoError(t, err)
+func TestSendEmailWithGamil(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
 
-// 	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
 
-// 	subject := "A test email"
-// 	content := `
-// 		<h1>Hello world</h1>
-// 		<p>hey hey hey</p>
-// 	`
-// 	to := []string{"????????@mail.com"}
-// 	attachFiles := []string{"../README.md"}
+	subject := "A test email"
+	content := `
+		<h1>Hello world</h1>
+		<p>hey hey hey</p>
+	`
+	to := []string{"????????@mail.com"}
+	attachFiles := []string{"../README.md"}
 
-// 	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
-// 	require.NoError(t, err)
-// }
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
