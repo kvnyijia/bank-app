709a8ee566a4a483bc4ea84433aea8abee5174b1
Yijia Chen <kevinc110604@gmail.com>
2023-08-25 17:54:00 -0700

    [new] Add mail/sender.go & unit test
    Complete L59

---
+++ mail/sender_test.go
@@ -0,0 +1,26 @@
+package mail
+
+// import (
+// 	"testing"
+
+// 	"github.com/kvnyijia/bank-app/util"
+// 	"github.com/stretchr/testify/require"
+// )
+
+// func TestSendEmailWithGamil(t *testing.T) {
+// 	config, err := util.LoadConfig("..")
+// 	require.NoError(t, err)
+
+// 	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+// 	subject := "A test email"
+// 	content := `
+// 		<h1>Hello world</h1>
+// 		<p>hey hey hey</p>
+// 	`
+// 	to := []string{"????????@mail.com"}
+// 	attachFiles := []string{"../README.md"}
+
+// 	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+// 	require.NoError(t, err)
+// }
